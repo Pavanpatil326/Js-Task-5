7db0f16707d4d2da11f2b09e43aa4963ed60bf01
Pavanpatil326 <143817491+Pavanpatil326@users.noreply.github.com>
2024-09-05 10:28:58 +0530

    Add Html And Js

---
+++ fifth.js
@@ -0,0 +1,16 @@
+/*let msg = "PavanPatil";
+
+let Newmsg = msg.trim();
+
+console.log(Newmsg.toUpperCase());  
+*/
+
+/*let Name = "ApnaCollege";
+  
+console.log(Name.slice(4,9));
+console.log(Name.indexOf("na"));
+console.log(Name.replace("Apna","Good"));
+*/
+
+let Name = "college";
+console.log(Name.replace("l","t"));
